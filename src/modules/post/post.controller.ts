2900e4f6f3ce92fb7d547d9af7f75b7b0e9d1119
Rubaid07 <rubaidindu@gmail.com>
2026-01-01 14:57:02 +0600

    Filtering Posts by Status and Author ID

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { postService } from "./post.service";
+import { PostStatus } from "../../../generated/prisma/enums";
 
 const createPost = async (req: Request, res: Response) => {
   try {
@@ -26,7 +27,17 @@ const getAllPost = async(req: Request, res: Response) => {
 
     const tags = req.query.tags ? (req.query.tags as string).split(",") : []
 
-    const result = await postService.getAllPost({search: searchString, tags})
+    const isFeatured = req.query.isFeatured 
+    ? req.query.isFeatured === 'true' 
+    ? true : req.query.isFeatured === 'false' 
+    ? false : undefined
+    : undefined
+
+    const status = req.query.status as PostStatus | undefined
+
+    const authorId = req.query.authorId as string | undefined
+
+    const result = await postService.getAllPost({search: searchString, tags, isFeatured, status, authorId})
     res.status(200).json(result)
   }catch (e) {
     res.status(404).json({
