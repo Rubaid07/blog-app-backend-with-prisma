7bcf1a0499b6983e6c5580cd7408225c811d015d
Rubaid07 <rubaidindu@gmail.com>
2026-01-02 21:07:02 +0600

    admin seeding

---
+++ src/scripts/seedAdmin.ts
@@ -0,0 +1,46 @@
+import { prisma } from "../lib/prisma";
+import { UserRole } from "../middlewares/auth";
+
+async function seedAdmin() {
+    try{
+        const adminData = {
+            name: "Admin1 bhai",
+            email: "admin1@admin.com",
+            role: UserRole.ADMIN,
+            password: "admin1234"
+        }
+        // check user exist on db or not
+        const existingUser = await prisma.user.findUnique({
+            where: {
+                email: adminData.email
+            }
+        });
+        if(existingUser){
+            throw new Error("User already exists!");
+        }
+
+        const signUpAdmin = await fetch("http://localhost:3000/api/auth/sign-up/email", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(adminData)
+        })
+
+        if(signUpAdmin.ok){
+            await prisma.user.update({
+                where: {
+                    email: adminData.email
+                },
+                data: {
+                    emailVerified: true
+                }
+            })
+        }
+
+    }catch(error){
+        console.error(error);
+    }
+}
+
+seedAdmin()
